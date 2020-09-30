da53bdb59fb6a2542adef61db7de90b0c4933a23
priyankarai11 <priyankakumbra@gmail.com>
2020-09-30 11:21:24 -0700

    Added:Sroring values in Dictionary

--- sort.sh
+++ sort.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+declare -A  dict
+
 echo "Arithmatic Computation and Sorting"
 echo "Enter 3 inputs"
 read a b c
@@ -11,3 +14,10 @@ echo "Answer for third formula =$y"
 z=$(((a%b)+c))
 echo "Answer for fourth formula =$z"
 
+echo "Storing in Dictionary"
+echo " 1 . a+b*c 2 .a*b+c 3 . c+a/b 4 . a%b+c"
+dict[1]=$w
+dict[2]=$x
+dict[3]=$y
+dict[4]=$z
+echo "Dictionary value =${dict[*]}"
